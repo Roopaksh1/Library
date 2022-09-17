660e98679e278ac1bab6ba1e2f65ccd169d5985e
Roopaksh <72032743+Roopaksh1@users.noreply.github.com>
2022-09-17 22:48:58 +0530

    Add form

--- script.js
+++ script.js
@@ -8,9 +8,23 @@ const addButton = () => {
 }
 
 const bindEvents = () => {
-
+  document.querySelector(".add-book").addEventListener("click", openForm);
 }
 
 window.addEventListener("load", addButton);
 window.addEventListener("load", bindEvents);
 
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+const addBookToLibrary = () => {
+
+}
+
+const openForm = () => {
+  
+}
